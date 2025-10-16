bae67faf23d717c0ad023049b1ad236e391c75b9
rahinsoo <rahinsoo@etik.com>
2025-10-16 17:22:56 +0200

    premier commit

---
+++ test.js
@@ -0,0 +1,6 @@
+let aujourdhui = new Date();
+let anniversaireJ = new Date(2018, 8, 22);
+let anniversaireJH = new Date(2018, 8, 22, 8, 20, 32);
+console.log(aujourdhui);
+console.log(anniversaireJ.toLocaleDateString());
+console.log(anniversaireJH);
